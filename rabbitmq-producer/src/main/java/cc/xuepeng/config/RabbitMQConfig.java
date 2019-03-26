334ab4068751bce86ae96de0e04a011966ce0b0b
XuePeng87 <penguin_1030@hotmail.com>
2019-03-26 23:43:31 +0800

    编写RabbitMQ的使用代码

---
+++ rabbitmq-producer/src/main/java/cc/xuepeng/config/RabbitMQConfig.java
@@ -0,0 +1,51 @@
+package cc.xuepeng.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
+import javax.annotation.Resource;
+
+/**
+ * RabbitMQ的配置对象。
+ *
+ * @author xuepeng
+ */
+public class RabbitMQConfig {
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    private static final Log LOGGER = LogFactory.getLog(RabbitMQConfig.class);
+
+    /**
+     * 定制AmqpTemplate对象。
+     * 可根据需要定制多个。
+     *
+     * @return AmqpTemplate对象。
+     */
+    public AmqpTemplate amqpTemplate() {
+        // 设置消息转换器为Jackson
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        rabbitTemplate.setEncoding("UTF-8");
+        // 不接受不可路由的消息，需要在yml中配置：publisher-returns: true
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                String correlationId = message.getMessageProperties().getCorrelationId();
+                LOGGER.info(String.format("消息 %s 发送失败，应答码：%s，原因：%s，交换机: %s，路由键：%s",
+                        correlationId,
+                        replyCode,
+                        replyText,
+                        exchange,
+                        routingKey));
+            }
+        });
+        return rabbitTemplate;
+    }
+
+
+}
